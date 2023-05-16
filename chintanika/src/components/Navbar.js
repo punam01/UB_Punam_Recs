e5ff846b0f4f9f11c85054ddc4735415404f1a88
Punam Kumavat <punamkumavat01@gmail.com>
2023-05-16 15:54:46 +0530

    UI: cursor styling

--- chintanika/src/components/Navbar.js
+++ chintanika/src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar(props){
         <>
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">{props.heading}</a>
+                <a className="navbar-brand mx-2" href="/">{props.heading}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar">
                     <IconContext.Provider value={{ className: "top-react-icons" }}>
