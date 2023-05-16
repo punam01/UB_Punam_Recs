5895910392cddc708ce74687ec33651b88e93f91
Punam Kumavat <punamkumavat01@gmail.com>
2023-05-16 19:56:41 +0530

    UI: InfiniteScroll component added

--- chintanika/src/App.js
+++ chintanika/src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import HeroBanner from './components/HeroBanner';
 import Navbar from './components/Navbar';
 import Trending from './components/Trending';
+import InfScroll from './components/InfScroll'
 function App() {
   return (
     <>
     <Navbar heading="Chintanika"></Navbar>
     <HeroBanner></HeroBanner>
-    <Trending>
-
-    </Trending>
+    <Trending></Trending>
+    <InfScroll></InfScroll>
     </>
   );
 }
---
+++ chintanika/src/components/InfScroll.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function InfScroll(props){
+    return(
+        <>
+        </>
+    );
+}
+export default InfScroll;
