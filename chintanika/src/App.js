f5a2803c445e56c8567ab427e44a936d56a75c89
Punam Kumavat <punamkumavat01@gmail.com>
2023-05-16 15:42:04 +0530

    UI: hero banner and cursor changed

--- chintanika/src/App.js
+++ chintanika/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import HeroBanner from './components/HeroBanner';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
     <Navbar heading="Chintanika"></Navbar>
+    <HeroBanner></HeroBanner>
     </>
   );
 }
---
+++ chintanika/src/components/HeroBanner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function HeroBanner() {
+  return (
+    <>
+    <section className=''>
+        <h1 className='hero-heading'>Stay Curious</h1>
+        <h4 className='my-2'>Share philosophical ideas, thoughts, 
+            and perspectives with others</h4>
+        <button type="button" class="btn btn hero shadow my-5 p-2">Start Reading</button>
+    </section>
+    </>
+  );
+}
+
+export default HeroBanner;
