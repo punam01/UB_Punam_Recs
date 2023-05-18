e2a768b57586465b75f47f0c4dd471c8344e76c0
Punam Kumavat <punamkumavat01@gmail.com>
2023-05-18 12:41:34 +0530

    feat: user model validated

--- chintanika/chintanika_backend/routes/article.js
+++ chintanika/chintanika_backend/routes/article.js
@@ -2,11 +2,12 @@ const express=require('express');
 const router=express.Router();
 const Article=require('../models/Article')
 
-router.get('/',(req,res)=>{
+router.post('/',(req,res)=>{
     console.log(req.body);
     const article=Article(req.body);
     console.log(res.body);
     article.save();
+    res.send(req.body);
 })
 
 module.exports=router
--- chintanika/chintanika_backend/routes/auth.js
+++ chintanika/chintanika_backend/routes/auth.js
@@ -1,11 +1,20 @@
 const express=require('express');
 const router=express.Router();
-const User=require('../models/User')
+const User=require('../models/User');
+const {body,validationResult}=require('express-validator');
 
-router.get('/',(req,res)=>{
-    console.log(req.body);
-    const user=User(req.body);
-    user.save();
+router.post('/'
+,[
+    body('name','name length should be minimum 5').isLength({min:5}),
+    body('email','email format ->name@test.com').isEmail(),
+    body('password','password length must be greater than 5').isLength({min:6})
+]
+,(req,res)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    res.send(req.body);
 })
 
 
