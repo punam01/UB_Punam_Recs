8ce686260f6272366fa2b4c6f5dc54cf9af0d3b0
Punam Kumavat <punamkumavat01@gmail.com>
2023-05-20 01:44:51 +0530

    feat: Login completed

--- chintanika/chintanika_backend/routes/auth.js
+++ chintanika/chintanika_backend/routes/auth.js
@@ -68,10 +68,11 @@ router.post(
     body("password", "password cannot be empty").exists(),
   ],
   async (req, res) => {
+    let success=false;
     //if there are error -->return bad request and error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({success, errors: errors.array() });
     }
     //otherwise get email or password from request body
     const { email, password } = req.body;
@@ -81,18 +82,20 @@ router.post(
       let user = await User.findOne({ email });
       //if user doesnot exists return user does not exists
       if (!user) {
+        success=false;
         return res
           .status(400)
-          .json({ error: "Login with correct credentials" });
+          .json({ success,error: "Login with correct credentials" });
       }
 
       //otherwise compare paswword entered by user with password in the table using bcrypt
       const passwordCompare = await bcrypt.compare(password, user.password);
       //if wrong password
       if (!passwordCompare) {
+        success=false;
         return res
           .status(400)
-          .json({ error: "Login with correct credentials" });
+          .json({ success,error: "Login with correct credentials" });
       }
       //else send authToken
       const data = {
@@ -101,7 +104,8 @@ router.post(
         },
       };
       const authToken = jwt.sign(data, JWT_SECRET);
-      res.json(authToken);
+      success=true;
+      res.status(400).json({success,authToken});
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal Server Error");
@@ -113,7 +117,7 @@ router.post(
 router.post("/getuser",fetchUser , async (req, res) => {
   //middleware function fetchUser is called whenever login required endpoints receive any requests
   try {
-    userid = req.user.id;
+    const userid = req.user.id;
     const user = await User.findById(userid).select("-password");
     res.send(user);
   } catch (error) {
--- chintanika/src/components/Login.js
+++ chintanika/src/components/Login.js
@@ -1,27 +1,75 @@
-import React from 'react'
-
-
-function Login(params) {
-    return(
-        <>
-        <section className='container my-5'>
-            <h5 className='hero-heading fs-1'>Login</h5>
-            <p>Let's get started</p>
-            <form>
-                <div className="mb-3 text-start col-6">
-                    <label htmlFor="exampleFormControlInput1" className="form-label is-valid">Email address</label>
-                    <input type="email" className="form-control w-30" id="exampleFormControlInput1" placeholder="name@example.com"/>
-                </div>
-                <div className="mb-3 text-start col-6">
-                    <label htmlFor="exampleFormControlInput1" className="form-label is-valid">Password</label>
-                    <input type="password" className="form-control w-30" id="exampleFormControlInput1" placeholder=""/>
-                </div>
-                <div className="my-3 col-6">
-                    <button className="btn hero" type="submit">Login</button>
-                </div>
-            </form>
-        </section>
-        </>
-    );
+import React, { useState } from "react";
+import {useNavigate } from "react-router-dom"
+const Login=(params)=> {
+    let navigate=useNavigate ();
+   const[credentials,setCredentials]=useState({
+    email:"",
+    password:""
+   });
+  const handleSubmit = async (e) => {
+    e.preventDefault(); //prevent from reload
+    const response = await fetch("http://localhost:5000/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+    },
+        body:JSON.stringify({email:credentials.email,password:credentials.password})
+    });
+    const json = await response.json();
+    console.log(json);
+    if(json.success){
+        //save auth token and redirect localstorage
+        localStorage.setItem('token',json.authToken);
+        navigate("/")
+    }
+    else{
+        alert("invalid cred");
+    }
+  };
+  const onChange=(e)=>{
+    setCredentials({...credentials,[e.target.name]:e.target.value})
+  }
+  return (
+    <>
+      <section className="container my-5">
+        <h5 className="hero-heading fs-1">Login</h5>
+        <p>Let's get started</p>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3 text-start col-6">
+            <label htmlFor="email" className="form-label is-valid">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control w-30"
+              id="email"
+              name="email"
+              placeholder="name@example.com"
+              value={credentials.email}
+              onChange={onChange}
+            />
+          </div>
+          <div className="mb-3 text-start col-6">
+            <label htmlFor="password" className="form-label is-valid">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control w-30"
+              id="password"
+              name="password"
+              value={credentials.password}
+              onChange={onChange}
+            />
+          </div>
+          <div className="my-3 col-6">
+            <button className="btn hero" type="submit" >
+              Login
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 export default Login;
