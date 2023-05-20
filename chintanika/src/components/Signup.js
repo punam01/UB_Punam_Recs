838a83cb17492787da1a889cf4f2445375a8797e
Punam Kumavat <punamkumavat01@gmail.com>
2023-05-20 11:35:47 +0530

    Update Signup.js

--- chintanika/src/components/Signup.js
+++ chintanika/src/components/Signup.js
@@ -4,32 +4,8 @@ import { Link } from 'react-router-dom';
 
 function Signup(params) {
 
-    const [data,setData]=useState(
-        {
-            uname:'',
-            email:'',
-            password:''
-        }
-    );
-
-    const [error,setError]=useState(
-        {
-            error:{},
-            isError:false
-        }
-    );
-
-    //dynamic value change
-    const handleChange=(e,field)=>{
-        setData({...data,[field]:e.target.value});
-    }
-
-    //submit form logic
-    const submitForm=(e)=>{
-        //prevent from reloading page
-        e.preventDefault();
-
-        //call api
+    const onChange=()=>{
+        
     }
 
     return(
@@ -37,37 +13,28 @@ function Signup(params) {
         <section className='container my-5'>
             <h5 className='hero-heading fs-1'>Create your account</h5>
             <p>Let's get started</p>
-            <form onSubmit={submitForm}>
+            <form onSubmit>
                <div className="mb-3 text-start col-6">
-                    <label htmlFor="exampleFormControlInput1" className="form-label">Name</label>
-                    <input type="text" className="form-control w-30" id="exampleFormControlInput1" placeholder=""
-                        onChange={(data)=>handleChange(data,"uname")}
-                        value={data.uname}
+                    <label htmlFor="uname" className="form-label">Name</label>
+                    <input type="text" className="form-control w-30" id="uname" placeholder="" 
+                        onChange={onChange}
                     />
                 </div>
                 <div className="mb-3 text-start col-6">
-                    <label htmlFor="exampleFormControlInput1" className="form-label is-valid">Email address</label>
-                    <input type="email" className="form-control w-30" id="exampleFormControlInput1" placeholder="name@example.com"
-                    onChange={(data)=>handleChange(data,"email")}
-                    value={data.email}
+                    <label htmlFor="email" className="form-label is-valid">Email address</label>
+                    <input type="email" className="form-control w-30" id="email" placeholder="name@example.com"
+                    onChange={onChange}
                     />
                 </div>
                 <div className="mb-3 text-start col-6">
-                    <label htmlFor="exampleFormControlInput1" className="form-label is-valid">Password</label>
-                    <input type="password" className="form-control w-30" id="exampleFormControlInput1" placeholder=""
-                    onChange={(data)=>handleChange(data,"password")}
-                    value={data.password}
+                    <label htmlFor="password" className="form-label is-valid">Password</label>
+                    <input type="password" className="form-control w-30" id="password" placeholder=""
+                    onChange={onChange}
                     />
                 </div>
                 <div className="mb-3 text-start col-6">
-                    <label htmlFor="exampleFormControlInput1" className="form-label is-valid">Confirm Password</label>
-                    <input type="password" className="form-control w-30" id="exampleFormControlInput1" placeholder=""/>
-                </div>
-                <div className="form-check col-6">
-                    <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" unchecked/>
-                    <label className="form-check-label" htmlFor="flexCheckChecked">
-                        I agree to all Terms, Privacy Policy and Fees
-                    </label>
+                    <label htmlFor="cpassword" className="form-label is-valid">Confirm Password</label>
+                    <input type="password" className="form-control w-30" id="cpassword" placeholder="" onChange={onChange}/>
                 </div>
                 <div className="my-3 col-6">
                     <Link className="btn hero" to="/login">Sign Up</Link>
